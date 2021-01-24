0a0bcabb33db99c617a99106580aa5ffdfc1fe32
libingfeng2 <libingfeng2006@sina.com>
2021-01-24 10:57:18 +0800

    单独推送测试文件

---
+++ Test.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.git;
+
+/**
+ * 李冰峰
+ * 2021/1/24 0024
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+}
